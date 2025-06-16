4dec5b03a2239853b1be67a4758842fdb19676f7
leelabakuri <leelaradhikabakuri@gmail.com>
2025-06-16 16:13:13 +0530

    Update and rename Ternaryoperators.java to Conditionaloperators.java

--- operators/Conditionaloperators.java
+++ operators/Conditionaloperators.java
@@ -1,4 +1,4 @@
-public class Ternaryoperators{
+public class Conditionaloperators{
     public static void main(String[] args) {
         int number = 15;
         String result = (number % 2 == 0) ? "Even" : "Odd";
