052fb9231ea82e5d625993ddc1d145fdaf64cc7c
leelabakuri <leelaradhikabakuri@gmail.com>
2025-06-16 16:08:10 +0530

    Add files via upload

---
+++ java/Assignmentoperators.java
@@ -0,0 +1,17 @@
+class Assignmentoperators
+{
+public static void main(String [] args)
+{
+int x=10;
+x +=5;
+System.out.println("x +=5:" +x);
+x -=5;
+System.out.println("x -=5:" +x);
+x *=5;
+System.out.println("x *=5:" +x);
+x /=5;
+System.out.println("x /=5:" +x);
+x %=5;
+System.out.println("x %=5:" +x);
+}
+}
---
+++ java/Ternaryoperators.java
@@ -0,0 +1,7 @@
+public class Ternaryoperators{
+    public static void main(String[] args) {
+        int number = 15;
+        String result = (number % 2 == 0) ? "Even" : "Odd";
+        System.out.println("The number " + number + " is " + result + ".");
+    }
+}
