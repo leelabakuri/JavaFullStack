bf0dfdea3bf496466fd2e7fe713d50aaccbbd953
leelabakuri <leelaradhikabakuri@gmail.com>
2025-06-16 16:09:33 +0530

    Delete java directory

--- java/Ternaryoperators.java
+++
@@ -1,7 +0,0 @@
-public class Ternaryoperators{
-    public static void main(String[] args) {
-        int number = 15;
-        String result = (number % 2 == 0) ? "Even" : "Odd";
-        System.out.println("The number " + number + " is " + result + ".");
-    }
-}
