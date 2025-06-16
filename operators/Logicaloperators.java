4b8005fadb800f1b01086f57cfa4ad1fa8083882
leelabakuri <leelaradhikabakuri@gmail.com>
2025-06-16 16:10:27 +0530

    Add files via upload

---
+++ operators/Logicaloperators.java
@@ -0,0 +1,10 @@
+class Logicaloperators
+{
+public static void main(String [] args)
+{
+boolean l=true,r=false;
+System.out.println("l&&r:" +(l&&r));
+System.out.println("l||r:" +(l||r));
+System.out.println("!r:" +(!r));
+}
+}
