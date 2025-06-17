b0799edd892d2e0127965467aec1923f3dac9a11
leelabakuri <leelaradhikabakuri@gmail.com>
2025-06-17 21:42:05 +0530

    Update PATTERNS

--- Patterns.java
+++ Patterns.java
@@ -181,20 +181,6 @@ output:
 10101
 */
 
-class Patterns
-{
-public static void main(String [] args)
-{
-for(int i=1;i<=5;i++)
-{
-for(int j=1;j<=5;j++)
-{
-System.out.print("*");
-}
-System.out.println();
-}
-}
-}
 
 
 
